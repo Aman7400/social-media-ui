3a4c151451f210b30fafd55fe4cd58f1dea512b9
Aman Shukla <amanks7400@gmail.com>
2022-08-06 01:03:27 +0530

    Add Explore Cards

--- src/screens/feature/Explore.js
+++ src/screens/feature/Explore.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Searchbar } from 'react-native-paper';
+import { Card, Searchbar } from 'react-native-paper';
+import { Dimensions, FlatList } from 'react-native';
 
 const Explore = () => {
 
@@ -15,8 +16,45 @@ const Explore = () => {
                 placeholder="Search"
                 onChangeText={onChangeSearch}
                 value={searchQuery}
+                style={{marginBottom:16}}
             />
+            {/* Explore Items */}
+            <FlatList showsVerticalScrollIndicator={false} scrollsToTop={true} numColumns={2} data={[{
+                id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+                title: "First Item",
+            },
+            {
+                id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+                title: "Second Item",
+            },
+            {
+                id: "58694a0f-3da1-471f-bd96-145571e29d72",
+                title: "Third Item",
+            },            {
+                id: "58694a0f-3da1-471f-bd96-145571e29d7",
+                title: "Third Item",
+            },            {
+                id: "58694a0f-3da1-471f-bd96-145571e292",
+                title: "Third Item",
+            },            {
+                id: "58694a0f-3da1-471f-bd96-1455729d72",
+                title: "Third Item",
+            },            {
+                id: "58694a0f-3da1-471f-bd96-145529d72",
+                title: "Third Item",
+            },]} keyExtractor={(item) => item.id} renderItem={({ item }) => <ExlporeItem />} />
         </SafeAreaView>
+    )
+}
+
+const ExlporeItem = () => {
+    return (
+        <Card style={{
+            width:(Dimensions.get("screen").width-32-16)/2,
+            margin:4,
+        }}>
+            <Card.Cover source={{ uri: 'https://picsum.photos/900' }} />
+        </Card>
     )
 }
 
