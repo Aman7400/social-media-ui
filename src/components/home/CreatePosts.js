5406add141facb138593f41a5035911e98283a30
Aman Shukla <amanks7400@gmail.com>
2022-08-05 23:28:00 +0530

    Home Create Posts

--- src/components/home/CreatePosts.js
+++ src/components/home/CreatePosts.js
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React from 'react'
+import { colors } from '../../theme/theme'
+import { FAB } from 'react-native-paper'
 
 const CreatePosts = () => {
-  return (
-    <View>
-      <Text>CreatePosts</Text>
-    </View>
-  )
+    return (
+        <View  style={styles.fab} >
+            <FAB
+                icon="pencil-outline"
+                style={{
+                    backgroundColor:colors.onPrimaryContainer
+                }}
+                onPress={() => console.log('Pressed')}
+            />
+        </View>
+    )
 }
 
+const styles = StyleSheet.create({
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
+    },
+  })
 export default CreatePosts
