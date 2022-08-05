ff687305e12a833d3bb1f4192aba5f5c8017a40b
Aman Shukla <amanks7400@gmail.com>
2022-08-06 00:38:07 +0530

    Update Stories.js

--- src/components/home/Stories.js
+++ src/components/home/Stories.js
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { colors, fonts } from "../../theme/theme"
+
+import Icon from "react-native-vector-icons/Ionicons"
+import { Avatar } from 'react-native-paper'
 
 const Stories = () => {
   return (
-    <View>
-      <Text>Stories</Text>
+    <View style={{ marginBottom: 16 }}>
+      <Text style={{ fontSize: 20, fontFamily: fonts.medium, color: colors.primary, marginBottom: 8 }}>Stories</Text>
+
+
+
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {/* Add Story Button */}
+        <TouchableOpacity onPress={() => alert("Upload Story")} style={{ width: 48, height: 48, borderRadius: 100, backgroundColor: colors.primary, justifyContent: 'center', alignItems: "center" }}>
+          <Icon name="add" suppressHighlighting={true} color={colors.onPrimary} size={36} />
+        </TouchableOpacity>
+        {/* Other Stories  */}
+
+        {
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, i) => <OthersStoryItem key={i} />)
+        }
+
+
+      </ScrollView>
+
+
+
     </View>
   )
 }
+
+
+const OthersStoryItem = () => (
+  <Avatar.Image size={48} style={{
+    marginHorizontal: 4
+  }} source={{ uri: 'https://picsum.photos/24' }} />
+);
+
 
 export default Stories
