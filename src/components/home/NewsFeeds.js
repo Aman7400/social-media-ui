14bd556c9964cebc689cc5771e89b5312c65e018
Aman Shukla <amanks7400@gmail.com>
2022-08-06 10:06:20 +0530

    Update NewsFeeds.js

--- src/components/home/NewsFeeds.js
+++ src/components/home/NewsFeeds.js
@@ -1,10 +1,5 @@
-import { View, Text, ScrollView,StyleSheet } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
-import { colors, fonts } from "../../theme/theme"
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { BottomSheet } from 'react-native-btr';
-import { useNavigation } from '@react-navigation/native';
 import PostCard from '../PostCard';
 
 
