528258848acb6b8b2c54798233454f3adf655674
Aman Shukla <amanks7400@gmail.com>
2022-08-06 10:13:19 +0530

    Update Stories.js

--- src/components/home/Stories.js
+++ src/components/home/Stories.js
@@ -1,9 +1,8 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { View, ScrollView, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
-import { colors, fonts } from "../../theme/theme"
+import { colors } from "../../theme/theme"
 
 import Icon from "react-native-vector-icons/Ionicons"
-import { Avatar } from 'react-native-paper'
 
 const Stories = () => {
   return (
@@ -11,19 +10,14 @@ const Stories = () => {
 
       <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
         {/* Add Story Button */}
-        <TouchableOpacity onPress={() => alert("Upload Story")} style={{ width: 48, height: 48, borderRadius: 100, backgroundColor: colors.primary, justifyContent: 'center', alignItems: "center" }}>
+        <TouchableOpacity onPress={() => alert("Upload Story")} style={{ width: 48,marginRight:4, height: 48, borderRadius: 100, backgroundColor: colors.primary, justifyContent: 'center', alignItems: "center" }}>
           <Icon name="add" suppressHighlighting={true} color={colors.onPrimary} size={36} />
         </TouchableOpacity>
         {/* Other Stories  */}
-
         {
           [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, i) => <OthersStoryItem key={i} i={i} />)
         }
-
-
       </ScrollView>
-
-
 
     </View>
   )
@@ -31,9 +25,15 @@ const Stories = () => {
 
 
 const OthersStoryItem = ({i}) => (
-  <TouchableOpacity onPress={() => alert(`Story ${i}`)}>
-    <Avatar.Image size={48} style={{
-      marginHorizontal: 4
+  <TouchableOpacity style={{
+    marginHorizontal:4,
+    height:48,width:48,borderRadius:100
+  }} onPress={() => alert(`Story ${i}`)}>
+    <Image style={{
+     borderWidth: 2,
+     borderColor: "red",
+     height:48,width:48,
+     borderRadius:100,
     }} source={{ uri: 'https://picsum.photos/24' }} />
   </TouchableOpacity>
 );
